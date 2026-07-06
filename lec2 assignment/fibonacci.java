78deea939e37d2a2285fa8839a8bb3daf4623324
jitesh-giri <jitishgiri@gmail.com>
2026-07-07 00:22:43 +0530

    Fivonacci series

---
+++ lec2 assignment/fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+
+         System.out.print("Enter the number of terms for Fibonacci series: ");
+
+         int n = in.nextInt();
+
+         int a = 0, b = 1, c;
+
+         System.out.print("Fibonacci series up to " + n + " terms: ");
+
+            for (int i = 1; i <= n; ++i){
+
+                System.out.print(a + " ");
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+    }
+}
