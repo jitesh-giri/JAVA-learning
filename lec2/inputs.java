f051a9e213cd7aa57414aff9ecef2ec8a987d039
jitesh-giri <jitishgiri@gmail.com>
2026-07-10 16:32:06 +0530

    name change

---
+++ lec2/inputs.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // System.out.print("Please enter your rollno: ");
+        // int rollno = input.nextInt();
+        // System.out.println("Your roll number is " + rollno);
+
+        float marks = input.nextFloat();
+        System.out.println(marks); 
+    }
+}
---
+++ lec2/primitive.java
@@ -0,0 +1,13 @@
+public class primitive {
+    public static void main(String[] args) {
+        
+        int rollno = 64;
+        char letter='r';
+        float marks = 98.65457f;
+        double largeDecimalNumbers = 485212.5874;
+        long largeInteger = 453445646244235L ;
+        boolean check = false;
+
+        String name = "Jitesh"; //string is not primitive
+    }
+}
