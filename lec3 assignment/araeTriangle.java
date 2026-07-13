d7fc43a6ae23b8b886b4923eb37d1326e481a0f7
jitesh-giri <jitishgiri@gmail.com>
2026-07-13 22:58:58 +0530

    areaTriangle

---
+++ lec3 assignment/araeTriangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class araeTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the height of the triangle: ");
+        float h = sc.nextFloat();
+
+        System.out.print("Enter the base of the triangle: ");
+        float b = sc.nextFloat(); 
+
+        double area = 0.5 * b * h;
+
+        System.out.print("Area of the trianglr is " + area);
+
+        sc.close();
+    }
+}
