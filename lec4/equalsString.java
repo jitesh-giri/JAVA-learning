2f4156e45f1d6770d556b097cd46ad6d16e58149
jitesh-giri <jitishgiri@gmail.com>
2026-07-15 23:58:26 +0530

    equalsString

---
+++ lec4/equalsString.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class equalsString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String fruit = sc.next();
+
+        if (fruit.equals("Mango")) {
+            System.out.println("King of fruits");
+        }
+        if (fruit.equals("Orange")) {
+            System.out.println("Round shaped orange coloured fruit");
+        }
+
+    }
+}
