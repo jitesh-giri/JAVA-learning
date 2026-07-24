29712700f0174b9af9971a62edf2370e74638a83
jitesh-giri <jitishgiri@gmail.com>
2026-07-25 00:02:05 +0530

    update

--- lec3 assignment/volumePrism.java
+++ lec3 assignment/volumePrism.java
@@ -4,8 +4,19 @@ public class volumePrism {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int h = sc.nextInt();
-        int 
-        int base area
+        System.out.print("Enter base of triangle: ");
+        double base = sc.nextDouble();
+
+        System.out.print("Enter height of triangle: ");
+        double height = sc.nextDouble();
+
+        System.out.print("Enter length of prism: ");
+        double length = sc.nextDouble();
+
+        double volume = 0.5 * base * height * length;   // volume = base area * length
+
+        System.out.println("Volume of Triangular Prism = " + volume);
+
+        sc.close();
     }
 }
