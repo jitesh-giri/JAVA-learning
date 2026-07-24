c8d90213cbbb1f88779a26425898a169737d572a
jitesh-giri <jitishgiri@gmail.com>
2026-07-24 23:59:54 +0530

    volumePrism

---
+++ lec3 assignment/volumePrism.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class volumePrism {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int 
+        int base area
+    }
+}
