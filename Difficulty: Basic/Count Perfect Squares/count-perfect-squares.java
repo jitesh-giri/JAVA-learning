e21d23e5066e9fc55e25384c28671ad25c3cfdb0
Jitesh Giri <jitishgiri@gmail.com>
2026-08-07 00:33:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Perfect Squares/count-perfect-squares.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int countSquares(int n) {
+        // code here
+        int count = 0;
+        int i = 1;
+        
+        while(i * i < n){
+            i++;
+            count++;
+        }
+        return count;
+        
+    }
+}
